da9bc7757b7a7aa3003276772d98e5e1c5f673b3
Michał Kosyk <gager33@gmail.com>
2020-10-11 12:34:29 +0200

    add a different navbar for the user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def current_user_email(current_user)
     current_user&.email
   end
+
+  def current_user_game_master?(current_user)
+    current_user.game_master?
+  end
 end
